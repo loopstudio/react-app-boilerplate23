31e486cfeb41136cd316deea2b7b7dc24bbc052e
ariaseugeniab <ariaseugeniab@gmail.com>
2023-01-30 12:32:58 -0300

    REACTBOIL-7 Add yarn and jest

---
+++ src/jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/.next/**",
+    "!**/coverage/**",
+    "!**/jest.config.js",
+    "!**/next.config.js",
+  ],
+};
+
+module.exports = config;
