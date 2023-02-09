7007eeaf53cefa72e3da9f3765c3bf4ae4e88067
FacuEM <facundomartinezesc@gmail.com>
2023-02-09 16:25:21 -0300

    Update test title

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home screen title', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to home screen/i);
   expect(linkElement).toBeInTheDocument();
