b2e7db1833dee5517cb3c6f4d4c5dc75f116e270
Tiago-Zarzavidjian <tzarzavidjian@loopstudio.dev>
2023-02-15 15:59:47 -0300

    [REACTBOIL-18] Update base font-size to use rem units in base 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 
 import AuthRouter from 'features/common/components/AuthRouter';
 import i18n from 'locales';
 import theme from 'theme';
 
+import { globalStyles } from 'App.styles';
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <I18nextProvider i18n={i18n}>
-        <BrowserRouter>
-          <AuthRouter />
-        </BrowserRouter>
-      </I18nextProvider>
-    </ThemeProvider>
+    <>
+      <Global styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <I18nextProvider i18n={i18n}>
+          <BrowserRouter>
+            <AuthRouter />
+          </BrowserRouter>
+        </I18nextProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
--- src/features/Home/Home.styles.ts
+++ src/features/Home/Home.styles.ts
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 const HomeWrapper = styled.div`
   margin-top: 20px;
   color: ${({ theme }) => theme.color.indigo200};
+  font-size: 2rem;
 `;
 
 export default HomeWrapper;
